6beca3cd1c936173ce67c094902f432493199e28
174381115 <37857104+174381115@users.noreply.github.com>
2018-09-03 23:04:24 +0800

    new project

---
+++ src/main/java/com/zhou/test/metric/App.java
@@ -0,0 +1,13 @@
+package com.zhou.test.metric;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
